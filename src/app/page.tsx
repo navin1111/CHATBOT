5a7155c3bd671e60e12d3a0c0b244432aed46787
navin1111 <navinrc2003@gmail.com>
2024-05-16 17:20:29 +0530

    sixth commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="w-full flex flex-col h-screen items-center bg-[#f0e9e1] ">
       <div className="w-full h-10 pt-8 flex flex-col justify-center items-center">
-        <span className="font-serif font-bold ">Ask Junior</span>
+        <span className="font-serif font-bold ">CHATBOT</span>
         
       </div>
 
